ebe475da09e5ddf6e466facccf341aea47516b95
Serhii Kuzmin <erngorn@gmail.com>
2025-08-15 16:22:42 +0300

    Release build v1.0.1

---
+++ packages/utils/dist/index.d.ts
@@ -0,0 +1,4 @@
+export declare function Component(constructor: Function): void;
+export declare function Attribute(): PropertyDecorator;
+export declare function Watch(attr: string): MethodDecorator;
+//# sourceMappingURL=index.d.ts.map
